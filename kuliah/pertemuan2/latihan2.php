95eb35e86abac77df3000c2697f9f43519eb6ad9
vicaasptyn <vicaseptyana30@gmail.com>
2025-02-17 17:40:48 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "Vica Septyana";
+$email = "vicasptyn@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "teknik informatika";
+$universitas = "universitas pasundan";
+echo "<h1>$nama</h1>";
+echo "<h4>$email</h4>";
+echo "<p>Hello, saya $nama saya adalah seorang $pekerjaan di jurusan $jurusan $universitas <p>";
+?>
---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+
+<?php
+$satu = "Topi";
+$dua = "Bundar";
+echo "<h2>$satu saya $dua, $dua $satu saya.</h2>";
+?>
+
+</body>
+</html>
