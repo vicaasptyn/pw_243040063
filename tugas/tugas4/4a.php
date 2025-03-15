b819af46aac51f34e5cd136c561033b29fe76a6d
vicaasptyn <vicaseptyana30@gmail.com>
2025-03-15 22:35:18 +0700

    selesai mengerjakan tugas4

---
+++ tugas/tugas4/4a.php
@@ -0,0 +1,5 @@
+<?php
+
+$kata = ["epan", "cipta", "akan", "u", "asa", "besok"];
+
+echo "Masa depanmu diciptakan oleh apa yang kau kerjakan hari ini, bukan " . $kata[5];
---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,28 @@
+<?php
+
+$hardware = ["Motherboard", "Processor", "Hard Disk", "PC Coller", "VGA Card", "SSD"];
+
+
+echo "<h3>Macam-macam perangkat keras komputer</h3>";
+echo "<ol>";
+foreach ($hardware as $item) {
+    echo "<li>$item</li>";
+}
+echo "</ol>";
+
+array_push($hardware, "Card Reader", "Modem");
+
+
+sort($hardware);
+
+echo "<h3>Macam-macam perangkat keras komputer baru</h3>";
+echo "<ol>";
+foreach ($hardware as $item) {
+
+    if ($item == "Card Reader" || $item == "Modem") {
+        echo "<li><u style='color:red;'>$item</u></li>";
+    } else {
+        echo "<li>$item</li>";
+    }
+}
+echo "</ol>";
